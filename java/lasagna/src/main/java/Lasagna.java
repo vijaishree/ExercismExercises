58fed56681d9997367c8a6d88e6548e8d6054ed2
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-04-18 14:33:26 +0530

    Lasagna completed

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,28 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
-
+    public int expectedMinutesInOven(){
+        return 40;
+    }
     // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int minutes){
+        int remainingMinutes = expectedMinutesInOven() - minutes ;
 
+        return  remainingMinutes ;
+    }
     // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layers){
+        int preparationTimeForOneLayer = 2;
 
+        int preparationTime = layers * preparationTimeForOneLayer;
+
+        return preparationTime;
+    }
     // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layers , int timeSpentInOven){
+        int preparationTime = preparationTimeInMinutes(layers);
+
+        int totalTime = preparationTime + timeSpentInOven ;
+
+        return totalTime ;
+    }
 }
