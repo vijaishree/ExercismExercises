791568f4803b3e5a264498dada79ffe218e757ae
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-04-19 12:36:26 +0530

    pythagorean triplet solved

--- java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,29 +1,67 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 class PythagoreanTriplet {
-
+    private int a,b,c ;
     PythagoreanTriplet(int a, int b, int c) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this .a =a;
+        this.b =b;
+        this.c = c;
+
     }
 
     static TripletListBuilder makeTripletsList() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new TripletListBuilder();
     }
 
     static class TripletListBuilder {
-
+    private int sum = 0;
+    int max = Integer.MAX_VALUE;
         TripletListBuilder thatSumTo(int sum) {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+           this.sum = sum ;
+           return this;
         }
 
         TripletListBuilder withFactorsLessThanOrEqualTo(int maxFactor) {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+            this.max = maxFactor;
+            return this;
         }
 
         List<PythagoreanTriplet> build() {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+            List<PythagoreanTriplet> tripletList = new ArrayList<>();
+            max = Math.min(max, sum / 2);
+            for (int c = 3; c <= max; c++) {
+                if (c > sum) break;
+                int cSquare = c * c;
+                for (int b = 2; b < c; b++) {
+                    if ((b + c) > sum) break;
+                    int bSquare = b * b;
+                    int a = sum - b - c;
+                    int aSquare = a * a;
+                    if (a < b && (aSquare + bSquare) == cSquare) {
+                        tripletList.add(0,new PythagoreanTriplet(a, b, c));
+                    }
+                }
+            }
+            return tripletList;
         }
 
+    }
+    @Override
+    public String toString() {
+        return String.format("(%d, %d, %d)", a, b, c);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof PythagoreanTriplet other)
+            return a == other.a && b == other.b && c == other.c;
+
+        return false;
     }
 
 }
